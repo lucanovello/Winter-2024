925f0b9c461221ad562068ca8d6a3a5247024631
Luca Novello <gnovello@myseneca.ca>
2024-03-28 14:10:51 -0400

    OPS102 - added lab6.sh

---
+++ OPS102NCC/lab6.sh
@@ -0,0 +1,17 @@
+firstName=$1
+lastName=$2
+echo "--------------------------"
+echo "First name is: $firstName"
+echo "Last name is: $lastName"
+echo "--------------------------"
+echo
+echo "--------------------------"
+echo -p "Update First Name: " firstName
+echo -p "Update Last Name: " lastName
+echo "--------------------------"
+echo
+echo "--------------------------"
+echo "First name is: $firstName"
+echo "Last name is: $lastName"
+date
+echo "--------------------------"
